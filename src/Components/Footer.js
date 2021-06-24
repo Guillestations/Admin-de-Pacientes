a24ba88f57039dabf8efe1eddbd108bbe754372c
Guillestations <guillejuanmanuel@gmail.com>
2021-06-24 09:05:05 -0300

    deploy app

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const Footer = () => {
+    return ( 
+
+        <footer>
+            <div className="footer">
+                <a href="#" target="_blank"><i class="fab fa-linkedin fa-2x mr-1"></i></a>
+                <a href="https://github.com/Guillestations?tab=projects" target="_blank"><i class="fab fa-github-square fa-2x mr-1"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-whatsapp-square fa-2x mr-1"></i></a>
+                <p class="lead">&copy; todos los derechos reservados Hay..! Caramba! :)</p>
+            </div>
+            
+        </footer>
+     );
+}
+ 
+export default Footer;
